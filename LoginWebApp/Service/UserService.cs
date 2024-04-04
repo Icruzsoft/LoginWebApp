00fbde85f27edad367e099571cd7ef365903585d
Isaac Cruz Cuevas <Icruzsoft@hotmail.com>
2024-04-04 06:44:36 -0600

    Mk4

--- LoginWebApp/Service/UserService.cs
+++ LoginWebApp/Service/UserService.cs
@@ -14,11 +14,13 @@ namespace LoginWebApp.Services
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IAuthenticationManager _authenticationManager;
+        private readonly ApplicationDbContext _dbContext; 
 
         public UserService()
         {
             _userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
             _authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            _dbContext = new ApplicationDbContext(); 
         }
 
         public ApplicationUser ValidateUser(string email, string password)
@@ -42,5 +44,52 @@ namespace LoginWebApp.Services
         {
             _authenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
         }
+
+      
+        public ApplicationUser FindByEmail(string email)
+        {
+            return _userManager.FindByEmail(email);
+        }
+
+        public IdentityResult RegisterClient(ApplicationUser user, string password)
+        {
+            var result = _userManager.Create(user, password);
+            if (result.Succeeded)
+            {
+                _userManager.AddToRole(user.Id, "Client");
+            }
+            return result;
+        }
+
+        public IdentityResult RegisterExpert(ApplicationUser user, string password)
+        {
+            var result = _userManager.Create(user, password);
+            if (result.Succeeded)
+            {
+                _userManager.AddToRole(user.Id, "Expert");
+            }
+            return result;
+        }
+
+        public ApplicationUser FindByToken(string token)
+        {
+           
+            return _dbContext.Users.FirstOrDefault(u => u.SecurityStamp == token);
+        }
+
+        public bool ConfirmEmail(ApplicationUser user)
+        {
+            user.EmailConfirmed = true;
+            var result = _userManager.Update(user);
+            return result.Succeeded;
+        }
+
+        public IdentityResult UpdateUser(ApplicationUser user)
+        {
+            return _userManager.Update(user);
+        }
+
+        
     }
 }
+
